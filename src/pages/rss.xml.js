b133ba139a36e494da1c7a4c48e9fac71060f6ff
Lander Costa <landercosta@gmail.com>
2025-04-25 13:51:18 -0300

    siteConfig file with name, author and description and passing this data to the Head component and to the RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,12 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { name, author, description } from '../../siteConfig.json';
 
 export async function GET(context) {
   return rss({
-    title: 'Title | Blog',
-    description: 'Example description',
+    title: `${name[0].toUpperCase() + name.toLowerCase().slice(1)} | Blog`,
+    description: description,
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>pt-br</language>`,
+    customData: `<language>pt-BR</language>`,
   });
 }
